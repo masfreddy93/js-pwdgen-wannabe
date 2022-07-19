03c395e7b2c6144968d9b213a9fef988501bd313
masfreddy93 <masieronicola@gmail.com>
2022-07-19 15:20:23 +0200

    add variables in js and h2 in html

---
+++ app.js
@@ -0,0 +1,8 @@
+//variabili e domande
+const nome = prompt("Qual'è il tuo nome?");
+console.log(nome);
+const cognome = prompt("Qual'è il tuo cognome?");
+console.log(cognome);
+const colorePreferito = prompt("Qual'è il tuo colore preferito?");
+console.log(colorePreferito);
+
